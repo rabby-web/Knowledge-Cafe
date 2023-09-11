6ff6d45d4436318e5d4d1421e1ffe34dde8d8d2d
rabby-web <rabby.webdeveloper@gmail.com>
2023-09-12 01:14:24 +0600

    fake data to json

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# React + Vite
+# Knowledge Cafe
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl text-center font-bold'>Hello</h1>
+        </div>
+    );
+};
+
+export default Header;
