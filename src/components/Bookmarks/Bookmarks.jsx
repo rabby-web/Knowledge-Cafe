f9425e6345bbdefbdb44ae3c41504cc1539c5314
rabby-web <rabby.webdeveloper@gmail.com>
2023-09-12 10:26:14 +0600

    setup to cafe

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -3,12 +3,13 @@ import Bookmark from '../Bookmark/Bookmark';
 
 const Bookmarks = ({bookmarks,readingTime}) => {
     return (
-        <div className="md:w-1/3">
+        <div className="md:w-1/3 m-3">
             <div className='bg-purple-200 text-purple-800  p-2 my-3 border border-purple-500 text-xl font-medium rounded-lg shadow'>
                 <h3>Reading Time: {readingTime}</h3>
             </div>
             <div className='bg-slate-200 rounded-lg p-2 my-3 border border-slate-300'>
-            <h2 className='text-xl font-bold'>Bookmarked Blog: {bookmarks.length} </h2>
+                
+            <h2 className='text-xl font-bold border border-b-slate-500'>Bookmarked Blog: {bookmarks.length} </h2>
             {
                 bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
             }
